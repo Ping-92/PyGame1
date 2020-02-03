3053e69ccfbbdd262d8688e07aac87c790e43d83
Ping-92 <ohhyongping@gmail.com>
2020-02-03 16:42:15 +0800

    updated settings for dimensions and colour

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings():
+    """class to store all setting for Ping's Alien Invasion"""
+
+    def __init__(self):
+        """initialise game's settings"""
+        # Screen settings
+        self.screen.width = 1920
+        self.screen.height = 1080
+        self.bg_colour = (0, 0, 255)
